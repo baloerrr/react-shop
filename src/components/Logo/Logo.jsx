198de26ec5ca85421ed73e15a3d2974bc616a304
baloerrr <99787895+baloerrr@users.noreply.github.com>
2023-10-28 10:19:07 +0700

    fix 404 page

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom'
 
 function Logo() {
   return (
-    <Link className="bg-transparent border-0 font-bold hover:-translate-y-2 transition-all delay-100  Sk-Modernist-Bold flex flex-col tracking-[3px] text-[18px]">
+    <Link
+      to={'/notfound404'}
+      className="bg-transparent border-0 font-bold hover:-translate-y-2 transition-all delay-100  Sk-Modernist-Bold flex flex-col tracking-[3px] text-[18px]"
+    >
       <p className="-mb-2 text-primeColor">Show</p>
       <p className="dark:text-textBase text-bgDark">CaseYuk👾.</p>
     </Link>
